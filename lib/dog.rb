9c070752e2c0f5d3667c6f6fadcb156df0abc3c7
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:16:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,19 @@ require 'pry'
 class Dog
   # code goes here
 
+  def initialize(name, owner, mood ="nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    save
+  end
+
+
+  def save
+    @@all << self
+  end
+  self.all
+    @@all
+  end
+
 end
