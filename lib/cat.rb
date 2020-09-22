bbc57fd0cfafcf81f3f2fab340fb33f266e0308e
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:15:06 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,4 +3,22 @@ require 'pry'
 class Cat
   # code goes here
 
+  attr_accessor :name, :owner, :mood
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
+
+
+
+  def save
+    @@all << self
+  end
+  self.all
+    @@all
+  end
+
 end
