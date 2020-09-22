49db690c6c85315aeae6b8a83bda28eeb3898570
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:18:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -16,4 +16,37 @@ class Owner
   def say_species
     puts "I am a #{species}."
   end
+
+  def cats
+
+  end
+
+  def dogs
+
+  end
+
+  def buy_cat
+
+  end
+
+  def buy_dog
+
+  end
+
+  def walk_dogs
+
+  end
+
+  def fee_cats
+
+  end
+
+  def sell_pets
+
+  end
+
+  def list_pets
+
+  end
+  
 end
