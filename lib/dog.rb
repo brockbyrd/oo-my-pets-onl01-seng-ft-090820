21044af270ff0d4a12eca5a74ddb8b0bd179c083
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:44:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,10 @@ require 'pry'
 class Dog
   # code goes here
 
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
   def initialize(name, owner, mood ="nervous")
     @name = name
     @owner = owner
