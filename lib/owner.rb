b896f94cc0162dc1428e5c35f3f37622dcf8786c
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:40:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,13 +4,20 @@ class Owner
   # code goes here
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
 
   def say_species
     "I am a #{species}."
+  end
+
+  self.all
+    @@all
   end
 
   def cats
