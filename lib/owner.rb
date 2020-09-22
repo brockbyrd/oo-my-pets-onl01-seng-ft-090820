1a156a3cc64202eccde1fecbeee71dcdcbeaec4b
Brock Byrd <brockbyrdd@gmail.com>
2020-09-22 20:07:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,10 +2,19 @@ require 'pry'
 
 class Owner
   # code goes here
-  attr_accessor :name
+  attr_accessor :name, :species
 
   def initialize(name)
     @name = name
     @name.freeze
+  end
+
+  def species(species = "human")
+    @species = species
+    @species.freeze
+  end
+
+  def say_species
+    puts "#{species}"
   end
 end
